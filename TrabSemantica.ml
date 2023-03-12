ee268d34dcb83aa60ec77908a4008626f55aa1e6
Kathleen Hoff <102632132+HoffKath@users.noreply.github.com>
2023-03-11 22:33:56 -0300

    Parte 2

--- TrabSemantica.ml
+++ TrabSemantica.ml
@@ -8,10 +8,10 @@ type tipo =
     TyInt
   | TyBool
   | TyFn    of tipo * tipo
-  | TyPair   of tipo * tipo 
+  | TyPair  of tipo * tipo 
   | TyList  of tipo 
-  | TyVar   of int 
   | TyMaybe of tipo
+  | TyId
   
 (*op ∈ {+, −, ∗, div, <, ≤, >, ≥, =, and, or}*)
 type op = sum 
@@ -39,21 +39,20 @@ type typeEnv = (ident * tipo) list
 | match e1 with nothing ⇒ e2 | just x ⇒ e3*)
 type expr = 
     Num     of int 
-  | True
-  | False 
-  | OpBi    of op
+  | Bool    of bool
+  | OpBi    of op    * expr * expr
   | If      of expr  * expr * expr
   | Var     of ident
   | App     of expr  * expr
-  | Fn      of ident * expr
-  | Let     of ident * expr  * expr 
-  | LetRec  of ident * ident * expr * expr
+  | Fn      of ident * tipo  * expr
+  | Let     of ident * tipo  * expr  * expr 
+  | LetRec  of expr  * ident * tipo  * expr * expr 
   | Pair    of expr  * expr
   | Fst     of expr
   | Snd     of expr 
-  | Nil
+  | Nil     of tipo
   | List    of expr  * expr
-  | Hd      of expr
+  | Hd      of expr  
   | Tl      of expr 
   | MatchL  of expr  * expr * expr
   | Just    of expr
@@ -71,23 +70,108 @@ let rec lookup a k: tipo option =
 let update (a: typeEnv) (k: ident) (i: tipo) : typeEnv =
   (k,i) :: a
   
-let rec typeinfer (g: typeEnv) (e: expr): tipo = 
+let rec typeinfer (a: typeEnv) (e: expr): tipo = 
   match e with 
     Num n -> TyInt
-  | True  -> TyBool
-  | False -> TyBool
+  | Bool b -> TyBool
   | OpBi (e1,e2, e3) ->
-      if e1 = sum then TyInt
-      else if e1 = sub then TyInt
-      else if e1 = mult then TyInt 
-      else if e1 = div then TyInt
-      else TyBool
+      let t2 = typeinfer a e2 in 
+      let t3 = typeinfer a e3 in
+      (match e1 with
+         sum ->
+           if t2 = TyInt && t3 = TyInt then
+             TyInt
+           else raise 
+       | sub ->
+           if t2 = TyInt && t3 = TyInt then
+             TyInt
+           else raise 
+       | mult ->
+           if t2 = TyInt && t3 = TyInt then
+             TyInt
+           else raise 
+       | div ->
+           if t2 = TyInt && t3 = TyInt then
+             TyInt
+           else raise 
+       | _ -> 
+           if eqType(e2) && eqType(e3) then
+             TyBool
+           else raise )
+             
   | If (e1, e2, e3) ->
-      let t1 = typerinfer g e1 in
+      let t1 = typerinfer a e1 in
       (match t1 with
          TyBool ->
-           let t2 = typeinfer g e2 in
-           let t3 = typeinfer g e3 in
+           let t2 = typeinfer a e2 in
+           let t3 = typeinfer a e3 in
            if t2 = t3 then t2
            else raise
+       | _ -> raise )
+      
+  | Var (e1) -> 
+      lookup a x
+      
+  | Pair (e1, e2) ->
+      let t1 = typeInfer a e1 in 
+      let t2 = typeInfer a e2 in 
+      if t1 = t2 then 
+        TyPair (t1, t2)
+          
+  | Fn (x, t, e2) -> 
+      let up = update a x t in
+      TyFn(t, typeInfer up e2)
+      
+  | App (e1, e2) ->  
+      let t1 = typeInfer a e1 in
+      let t2 = typeInfer a e2 in
+      (match t1 with 
+         TyFn (t_in, t_out) ->
+           if t2 = t_in then 
+             t_out
+           else raise
+       | _ -> raise)
+      
+  | Fst (e1) ->
+      let t1 = typeInfer a e1 in
+      (match t1 with
+         TyPair (t1, t2) -> t1
+       | _ -> raise)
+      
+  | Snd (e1) ->
+      let t1 = typeInfer a e1 in 
+      (match t1 with
+         TyPair (t1, t2) -> t2
+       | _ -> raise)
+      
+  | Let (x, t, e1, e2) -> 
+      let tx = typeInfer a x in
+      let up = update a x tx in
+      let t1 = typeInfer a e1 in
+      if t = t1 then 
+        typeInfer up e2
+      else raise
   
+  | LetRec (f, x, t, e1, e2) -> 
+      
+      
+    
+                 
+  | Nil (t)-> TyList (t)
+  | List (e1, e2) ->
+      let t1 = typeInfer a e1 in
+      let t2 = typeInfer a e2 in 
+      if t2 = TyList (t1) then 
+        TyList (t1)
+  
+  | Hd (e1) ->
+      let t1 = typeInfer a e1 in 
+      if t1 = 
+  | Tl      of expr 
+  | MatchL  of expr  * expr * expr
+  | Just    of expr
+  | Nothing
+  | MatchJ  of expr  * expr * expr
+  | raise 
+  
+         (*fazer eqTYPE*)
